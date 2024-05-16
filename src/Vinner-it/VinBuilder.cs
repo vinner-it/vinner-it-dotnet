53080054cf8a227744aff097747a4d2d1854d391
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2024-05-16 18:05:33 +0200

    rename testfile
    move vintype to own file, move internal concern to vintype

--- src/Vinner-it/VinBuilder.cs
+++ src/Vinner-it/VinBuilder.cs
@@ -1,51 +1,18 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
 
 namespace Vinner_it;
 
 public class VinBuilder
 {
-    public static bool FastVin([NotNullWhen(true)] string? vin)
-    {
-        if (vin is null) return false;
-
-        var vinRegex = "^[A-HJ-NPR-Z0-9]{17}$";
-
-        try
-        {
-            return Regex.IsMatch(vin, vinRegex, RegexOptions.None, TimeSpan.FromMilliseconds(250));
-        }
-        catch (RegexMatchTimeoutException e)
-        {
-            return false;
-        }
-    }
-
     public static bool TryParseVin(string? input, [NotNullWhen(true)] out FastVin? vin)
     {
-        if (FastVin(input))
+        if (FastVin.IsValid(input))
         {
-            vin = Vinner_it.FastVin.Create(input);
+            vin = FastVin.Create(input);
             return true;
         }
 
         vin = null;
         return false;
-    }
-}
-
-public record FastVin
-{
-    public string Vin { get; }
-
-    private FastVin(string vin)
-    {
-        Vin = vin;
-    }
-    internal static FastVin Create(string vin)
-    {
-        // TODO: how do we make sure that serializers are not able to highjack validation-flow?
-        if (!VinBuilder.FastVin(vin)) throw new ArgumentException($"Input {nameof(vin)} is not valid");
-        return new FastVin(vin);
     }
 }
--- tests/Vinner-it.Tests/VinBuilderTests.cs
+++ tests/Vinner-it.Tests/VinBuilderTests.cs
@@ -2,44 +2,36 @@ using Vinner_it;
 
 namespace UnitTests;
 
-public class Tests
+public class VinBuilderTests
 {
     [Fact]
     public void Fast_VIN_Should_Return_True_If_Valid()
     {
-        var vinBuilder = new VinBuilder();
-        
-        var result = VinBuilder.FastVin("KNDPB3A21B7082471");
         var tryResult = VinBuilder.TryParseVin("KNDPB3A21B7082471", out var vin);
         
         Assert.True(tryResult);
         Assert.NotNull(vin);
         Assert.NotEmpty(vin.Vin);
-        Assert.True(result);
     }
     
     [Fact]
     public void Fast_VIN_Should_Return_False_If_Invalid()
     {
         
-        var result = VinBuilder.FastVin("KB3A21B7082471");
         var tryCreat = VinBuilder.TryParseVin("KB3A21B7082471", out var vin);
 
         Assert.False(tryCreat);
         Assert.Null(vin);
-        Assert.False(result);
     }
     
     [Fact]
     public void Fast_VIN_Should_Return_False_If_Null()
     {
         
-        var result = VinBuilder.FastVin(null);
         var tryCreate = VinBuilder.TryParseVin(null, out var vin);
 
         Assert.False(tryCreate);
         Assert.Null(vin);
-        Assert.False(result);
     }
 }
 
