85acf6b0180858648cc1c00af0997b0bd615ef49
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2024-05-16 16:46:08 +0200

    fix tests

--- tests/Vinner-it.Tests/Tests.cs
+++ tests/Vinner-it.Tests/Tests.cs
@@ -9,28 +9,36 @@ public class Tests
     {
         var vinBuilder = new VinBuilder();
         
-        var result = vinBuilder.FastVin("KNDPB3A21B7082471");
-
+        var result = VinBuilder.FastVin("KNDPB3A21B7082471");
+        var tryResult = VinBuilder.TryParseVin("KNDPB3A21B7082471", out var vin);
+        
+        Assert.True(tryResult);
+        Assert.NotNull(vin);
+        Assert.NotEmpty(vin.Vin);
         Assert.True(result);
     }
     
     [Fact]
     public void Fast_VIN_Should_Return_False_If_Invalid()
     {
-        var vinBuilder = new VinBuilder();
         
-        var result = vinBuilder.FastVin("KB3A21B7082471");
+        var result = VinBuilder.FastVin("KB3A21B7082471");
+        var tryCreat = VinBuilder.TryParseVin("KB3A21B7082471", out var vin);
 
+        Assert.False(tryCreat);
+        Assert.Null(vin);
         Assert.False(result);
     }
     
     [Fact]
     public void Fast_VIN_Should_Return_False_If_Null()
     {
-        var vinBuilder = new VinBuilder();
         
-        var result = vinBuilder.FastVin(null);
+        var result = VinBuilder.FastVin(null);
+        var tryCreate = VinBuilder.TryParseVin(null, out var vin);
 
+        Assert.False(tryCreate);
+        Assert.Null(vin);
         Assert.False(result);
     }
 }
