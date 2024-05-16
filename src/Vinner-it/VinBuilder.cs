4810c48d826a2a7cf92024161ccdae7b04b7c9cc
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2024-05-16 16:45:25 +0200

    refactor vin builder

---
+++ src/Vinner-it/VinBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace Vinner_it;
+
+public class VinBuilder
+{
+    public static bool FastVin([NotNullWhen(true)] string? vin)
+    {
+        if (vin is null) return false;
+
+        var vinRegex = "^[A-HJ-NPR-Z0-9]{17}$";
+
+        try
+        {
+            return Regex.IsMatch(vin, vinRegex, RegexOptions.None, TimeSpan.FromMilliseconds(250));
+        }
+        catch (RegexMatchTimeoutException e)
+        {
+            return false;
+        }
+    }
+
+    public static bool TryParseVin(string? input, [NotNullWhen(true)] out ValidVin? vin)
+    {
+        if (FastVin(input))
+        {
+            vin = new ValidVin(input);
+            return true;
+        }
+
+        vin = null;
+        return false;
+    }
+}
+
+public record ValidVin
+{
+    public string Vin { get; init; }
+
+    internal ValidVin(string vin)
+    {
+        // TODO: how do we make sure that serializers are not able to highjack validation-flow?
+        if (!VinBuilder.FastVin(vin)) throw new ArgumentException("Input {0} is not a valid vin", nameof(vin));
+        Vin = vin;
+    }
+}
