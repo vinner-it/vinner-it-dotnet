edbfdf3bf9fefbcaf251213d66ddf8ac946b419d
Atkinstinkmoore <44531198+Atkinstinkmoore@users.noreply.github.com>
2024-05-17 08:26:59 +0200

    add validation attr
    move classes to own files

---
+++ src/Vinner-it/VinBuilderProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Vinner_it;
+
+public class VinBuilderProvider
+{
+    private ImmutableList<Func<string?, bool>> _validators =
+        ImmutableList<Func<string?, bool>>.Empty;
+
+    private Func<string, FastVin> Factory = FastVin.Create;
+
+    public VinBuilderProvider ValidateRegex()
+    {
+        AddValidator(FastVin.IsValid);
+        return this;
+    }
+
+    public VinBuilder Build() => VinBuilder.Create(Factory, _validators.Distinct().ToImmutableList());
+
+    private void AddValidator(Func<string?, bool> validator) => _validators = _validators.Add(validator);
+}
+
+public class VinBuilder
+{
+    private readonly ImmutableList<Func<string?, bool>> _validators;
+    private readonly Func<string, FastVin> _factory;
+
+    private VinBuilder(Func<string, FastVin> factory, ImmutableList<Func<string?, bool>> validators)
+    {
+        _validators = validators;
+        _factory = factory;
+    }
+
+    internal static VinBuilder Create(Func<string, FastVin> factory, ImmutableList<Func<string?, bool>> validators)
+    {
+        return new VinBuilder(factory, validators);
+    }
+
+    public static bool TryParseVin(string? input, [NotNullWhen(true)] out FastVin? vin)
+    {
+        if (FastVin.IsValid(input))
+        {
+            vin = FastVin.Create(input);
+            return true;
+        }
+
+        vin = null;
+        return false;
+    }
+    public bool TryCreate(string? input, [NotNullWhen(true)] out FastVin? vin)
+    {
+        if (Validate(input))
+        {
+            vin = _factory(input);
+            return true;
+        }
+
+        vin = null;
+        return false;
+    }
+
+    public bool Validate([NotNullWhen(true)] string? vin) => _validators.All(f => f(vin));
+}
--- tests/Vinner-it.Tests/VinBuilderTests.cs
+++ tests/Vinner-it.Tests/VinBuilderTests.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 using Vinner_it;
 
 namespace UnitTests;
@@ -8,26 +10,26 @@ public class VinBuilderTests
     public void Fast_VIN_Should_Return_True_If_Valid()
     {
         var result = VinBuilder.TryParseVin("KNDPB3A21B7082471", out var vin);
-        
+
         Assert.True(result);
         Assert.NotNull(vin);
         Assert.NotEmpty(vin.Vin);
     }
-    
+
     [Fact]
     public void Fast_VIN_Should_Return_False_If_Invalid()
     {
-        
+
         var result = VinBuilder.TryParseVin("KB3A21B7082471", out var vin);
 
         Assert.False(result);
         Assert.Null(vin);
     }
-    
+
     [Fact]
     public void Fast_VIN_Should_Return_False_If_Null()
     {
-        
+
         var tryCreate = VinBuilder.TryParseVin(null, out var vin);
 
         Assert.False(tryCreate);
@@ -37,20 +39,94 @@ public class VinBuilderTests
     [Fact]
     public void Validate_WithRegex_ShouldReturnFalse()
     {
-        var builder = new VinBuilder().ValidateRegex().Build();
+        var builder = new VinBuilderProvider().ValidateRegex().Build();
 
         var result = builder.Validate("test");
-        
+        var valid = builder.TryCreate("test", out var vin);
+
         Assert.False(result);
+        Assert.False(valid);
+        Assert.Null(vin);
     }
 
     [Fact]
     public void Validate_WithValidRegexVin_ShouldReturnTrue()
     {
-        var builder = new VinBuilder().ValidateRegex().Build();
+        var builder = new VinBuilderProvider().ValidateRegex().Build();
 
         var result = builder.Validate("KNDPB3A21B7082471");
+        var valid = builder.TryCreate("KNDPB3A21B7082471", out var vin);
+
+        Assert.True(result);
+        Assert.True(valid);
+        Assert.NotNull(vin);
+        Assert.NotEmpty(vin.Vin);
+    }
+
+    [Fact]
+    public void ValidationAttribute_WithInvalidVin_ShouldReturnFalse()
+    {
+        var input = new TestClass()
+        {
+            VinString = "test"
+        };
+        var context = new ValidationContext(input);
+        var validationErrors = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(input, context, validationErrors, true);
+
+        Assert.False(isValid);
+
+    }
+
+    [Fact]
+    public void ValidationAttribute_WithValidVin_ShouldReturnTrue()
+    {
+
+        var input = new TestClass()
+        {
+            VinString = "YS2PB3A21B7082471",
+        };
+        var context = new ValidationContext(input);
+        var validationErrors = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(input, context, validationErrors, true);
+
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void ValidationAttribute_WithOptionalVin_ShouldReturnTrue()
+    {
+        var input = new TestClass() { VinString = null };
+        var validationErrors = new List<ValidationResult>();
+
+        var result = Validator.TryValidateObject(input, new ValidationContext(input), validationErrors, true);
         
         Assert.True(result);
     }
+
+    [Fact]
+    public void ValidationAttribute_WithInvalidVin_WhenDeserializing_ShouldReturnFalse()
+    {
+        var json = """{"VinString":"YS2PB3A2IB7082471"}""";
+
+        var result = JsonSerializer.Deserialize<TestClass>(json);
+
+        var context = new ValidationContext(result, null);
+        var validationErrors = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(result, context, validationErrors, true);
+
+        Assert.NotNull(result);
+        Assert.False(isValid);
+        Assert.Single(validationErrors);
+    }
+
+}
+
+public class TestClass
+{
+    [FastVin]
+    public string? VinString { get; set; }
 }
